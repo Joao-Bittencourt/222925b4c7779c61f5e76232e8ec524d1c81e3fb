15b1d6f32b8859bf97917a2de9eb63f5f84a3852
joao bittencourt <joaogsbittencourt@gmail.com>
2021-08-23 00:13:58 -0300

    Adicionado geração dos arquivos (em desenv). Adicionado citação do projeto original.

--- src/Common/Arquivo.php
+++ src/Common/Arquivo.php
@@ -7,6 +7,8 @@ use \stdClass;
 
 abstract class Arquivo implements ArquivoInterface {
 
+    const PAF_EOL = "\r\n";
+
     public $std;
     public $values;
     protected $parameters;
@@ -25,18 +27,106 @@ abstract class Arquivo implements ArquivoInterface {
         if (empty($this->parameters)) {
             throw new Exception('Parametros não estabelecidos na classe');
         }
-        
-        foreach ($this->parameters as $paramenter) {
-            print_r($paramenter);            die();
+        $newStd = new stdClass();
+        foreach ($this->parameters as $parameter) {
+
+            if (empty($std->$parameter)) {
+                continue;
+            }
+
+            switch (true) {
+                case is_array($std->$parameter) :
+                    $newStd->{$parameter} = $this->arBuildElement($parameter, $std);
+                    break;
+                default :
+                    $newStd->{$parameter} = $this->buildElement($parameter, $std);
+            }
         }
-        $newStd = $std;
-        
+
+        $this->values = $newStd;
+
         return $newStd;
     }
-    
-    protected function  buildElement($element, \stdClass $std) {
-        
+
+    protected function buildElement($element, \stdClass $std) {
+
+        switch ($element) {
+            case 'U1':
+            case 'A2':
+            case 'P2':
+            case 'E2':
+            case 'D2':
+            case 'D3':
+            case 'D4':
+            case 'S2':
+            case 'S3':
+            case 'J1':
+            case 'J2':
+            case 'U1':
+                $ClassElement = 'IsmaelAdriano\PAFNFCe\Elements\\' . $element;
+                if (class_exists($ClassElement)) {
+                    return new $ClassElement($std->$element);
+                }
+                throw new Exception("Elements::{$element} não existe.");
+        }
+
+        return $std;
     }
-    
+
+    protected function arBuildElement($element, \stdClass $std) {
+        $arElements = [];
+        switch ($element) {
+            case 'U1':
+            case 'A2':
+            case 'P2':
+            case 'E2':
+            case 'D2':
+            case 'D3':
+            case 'D4':
+            case 'S2':
+            case 'S3':
+            case 'J1':
+            case 'J2':
+            case 'U1':
+                $ClassElement = 'IsmaelAdriano\PAFNFCe\Elements\\' . $element;
+                if (class_exists($ClassElement)) {
+
+
+                    foreach ($std->$element as $elementKey => $elementData) {
+
+                        $arElements[] = new $ClassElement($std->$element[$elementKey]);
+                    }
+
+                    return $arElements;
+                }
+                throw new Exception("Elements::{$element} não existe.");
+        }
+
+        return $std;
+    }
+
+    public function geraArquivo() {
+        if (empty($this->values)) {
+            throw new Exception("Propriedade values não preenchido.");
+        }
+
+        $data = [];
+        foreach ($this->values as $element) {
+
+
+            if (is_array($element)) {
+                foreach ($element as $elementData) {
+                    $data[] = "{$elementData}";
+                }
+                continue;
+            }
+            $data[] = "{$element}";
+        }
+
+        $data = implode(self::PAF_EOL, $data);
+        $data .= self::PAF_EOL;
+
+        return $data;
+    }
 
 }
--- src/tests/ArquivoITest.php
+++ src/tests/ArquivoITest.php
@@ -10,12 +10,82 @@ class ArquivoITest extends TestCase {
 
     public function testArquivoI() {
         $std = new stdClass();
+        $std->U1 = new stdClass();
+        $std->U1->CNPJ = '26607235000130';
+        $std->U1->IE = '1257775242';
+        $std->U1->IM = '';
+        $std->U1->RAZAO_SOCIAL = 'Teste NFCe SC';
+        
+        $std->A2[0] = new stdClass();
+        $std->A2[0]->DATA = '27022021';
+        $std->A2[0]->MEIO_PAGAMENTO = 'Dinheiro';
+        $std->A2[0]->CODIGO_DOCUMENTO = '1';
+        $std->A2[0]->VALOR = 31000;
+        $std->A2[0]->CNPJ = '';
+        $std->A2[0]->NUMERO_DOCUMENTO = 0;
+        
+        $std->A2[1] = new stdClass();
+        $std->A2[1]->DATA = '27022021';
+        $std->A2[1]->MEIO_PAGAMENTO = 'Cheque';
+        $std->A2[1]->CODIGO_DOCUMENTO = '2';
+        $std->A2[1]->VALOR = 32000;
+        $std->A2[1]->CNPJ = '';
+        $std->A2[1]->NUMERO_DOCUMENTO = 1;
+        
+        $resp = new ArquivoI($std);
+        
+        $respU1 = ("{$resp->std->U1}");
+        $expectedU1 = 'U1266072350001301257775242                  Teste NFCe SC                                     ';
+       
+        $respA2Ar0 = ("{$resp->std->A2[0]}");
+        $expectedA2Ar0 = 'A227022021Dinheiro                 1000000031000              0000000000';
+        
+        $respA2Ar1 = ("{$resp->std->A2[1]}");
+        $expectedA2Ar1 = 'A227022021Cheque                   2000000032000              0000000001';
+        
+        $this->assertEquals($expectedU1, $respU1);
+        $this->assertEquals($expectedA2Ar0, $respA2Ar0);
+        $this->assertEquals($expectedA2Ar1, $respA2Ar1);
 
-        $b1 = new ArquivoI($std);
-        $resp = $b1;
-        print_r($resp);
-//        $this->assertIsObject($resp);
-//        $this->assertCount(8,);
     }
 
+    public function testGeraArquivo() {
+        $std = new stdClass();
+        $std->U1 = new stdClass();
+        $std->U1->CNPJ = '26607235000130';
+        $std->U1->IE = '1257775242';
+        $std->U1->IM = '';
+        $std->U1->RAZAO_SOCIAL = 'Teste NFCe SC';
+        
+        $std->A2[0] = new stdClass();
+        $std->A2[0]->DATA = '27022021';
+        $std->A2[0]->MEIO_PAGAMENTO = 'Dinheiro';
+        $std->A2[0]->CODIGO_DOCUMENTO = '1';
+        $std->A2[0]->VALOR = 31000;
+        $std->A2[0]->CNPJ = '';
+        $std->A2[0]->NUMERO_DOCUMENTO = 0;
+        
+        $std->A2[1] = new stdClass();
+        $std->A2[1]->DATA = '27022021';
+        $std->A2[1]->MEIO_PAGAMENTO = 'Cheque';
+        $std->A2[1]->CODIGO_DOCUMENTO = '2';
+        $std->A2[1]->VALOR = 32000;
+        $std->A2[1]->CNPJ = '';
+        $std->A2[1]->NUMERO_DOCUMENTO = 1;
+        
+        $resp = new ArquivoI($std);
+                
+        $arquivo = $resp->geraArquivo();
+        
+        $expected = [];
+        $expected[] = 'U1266072350001301257775242                  Teste NFCe SC                                     ';
+        $expected[] = 'A227022021Dinheiro                 1000000031000              0000000000';
+        $expected[] = 'A227022021Cheque                   2000000032000              0000000001';
+        $expected = implode(PHP_EOL, $expected);
+        $expected .= PHP_EOL;
+        
+        
+        $this->assertEquals($expected, $arquivo);
+
+    }
 }
